a49f9d11f29ef4b42b047f0ed698ff71bd34f009
Leonardo <leonardo.blanco@uabc.edu.mx>
2021-02-16 20:52:08 -0800

    Commit Inicial

---
+++ numeroImpar_o_par.py
@@ -0,0 +1,11 @@
+print("Odd or even")
+numero=0
+
+#A continuación solicitamos algún numero
+numero=int(input("¡¡¡¡Introduzca un número!!!"))
+
+#A continuación ponemos la condición
+if numero%2 == 0:
+    print("El número ", numero, " es par")
+else:
+        print("El número ", numero," es impar")
